97efb8d7b71fd12ac9762f4aaaa9fa3adad28b30
Tobias <tobias@terstad.se>
2015-10-28 16:07:03 +0000

    kontakt med gps

--- test/AT.py
+++ test/AT.py
@@ -1,9 +1,37 @@
 __author__ = 'tobias'
 
 import serial
+import time
 
-ser = serial.Serial(port='/tty/ttyAMA0', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=1, xonoff=False, rtscts=False, dsrdtr=False)
-cmd = "AT\r"
-ser.write(cmd.encode())
-msg = ser.read(64)
-print(msg)
+def sendCommand(com):
+    ser.write(com+"\r\n")
+    time.sleep(2)
+    ret = []
+    while ser.inWaiting() > 0:
+        msg = ser.readline().strip()
+        msg = msg.replace("\r","")
+        msg = msg.replace("\n","")
+        if msg!="":
+            ret.append(msg)
+    return ret
+
+#ser = serial.Serial(port='/dev/ttyAMA0', baudrate=115200, bytesize=8, parity='N', stopbits=1)
+ser = serial.Serial(port='/dev/ttyAMA0', baudrate=115200, timeout = 1)
+ser.close()
+ser.open()
+print("isOpen:"+str(ser.isOpen()))
+print("open serial")
+
+print(sendCommand("AT"))
+
+# GPS 
+gps = sendCommand("AT+CGPSINF=0")
+print("GPS:")
+print(gps)
+
+# Signal quality
+q = sendCommand("AT+CSQ")
+print("Signal:")
+print(q)
+
+ser.close()
