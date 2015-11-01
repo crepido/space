31c8f776cd2050a1c4399cc8507b75889db12dc9
Glenn <glenn.lundberg@crepido.com>
2015-11-01 12:20:28 +0100

    First version of working API for get and post

---
+++ SpaceshipTracker/src/SpaceshipTracker.Web/IPositionRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SpaceshipTracker.Web.Models;
+
+namespace SpaceshipTracker.Web.Repositories
+{
+    public interface IPositionRepository
+    {
+        IList<Position> GetAll();
+        IEnumerable<Position> GetAllByIdentifier(string identifier);
+        void Save(Position position);
+    }
+}
---
+++ SpaceshipTracker/src/SpaceshipTracker.Web/Repositories/PositionRepository.cs
@@ -0,0 +1,34 @@
+﻿using SpaceshipTracker.Web.Models;
+using SpaceshipTracker.Web.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceshipTracker.Web.Repositories
+{
+    public class PositionRepository : IPositionRepository
+    {
+        private readonly SpaceshipTrackerContext _ctx;
+
+        public PositionRepository(SpaceshipTrackerContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IList<Position> GetAll()
+        {
+            return _ctx.Positions.ToList();
+        }
+
+        public void Save(Position position)
+        {
+            _ctx.Positions.Add(position);
+            _ctx.SaveChanges();
+        }
+
+        public IEnumerable<Position> GetAllByIdentifier(string identifier)
+        {
+            return _ctx.Positions.Where(p=>p.Identifier.Equals(identifier)).ToList();
+        }
+    }
+}
