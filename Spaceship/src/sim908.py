9d2fe439d0000d30192ad053b510031078e65e58
Tobias Terstad <tobias@terstad.se>
2015-11-09 20:13:24 +0100

    Signal level fix

--- Spaceship/src/sim908.py
+++ Spaceship/src/sim908.py
@@ -224,7 +224,7 @@ class Sim908:
                 logging.debug(">10")
 
             return True
-        except RuntimeError or IndexError or ValueError:
+        except (RuntimeError, IndexError, ValueError):
             logging.error("Failed to check online status")
             return False
         finally:
